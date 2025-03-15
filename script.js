4b41f0eb5cc942d908e34fed19593db240d82f15
rksudhir <rksudhir@hotmail.com>
2025-03-15 00:12:46 -0400

    Updated to correct issues

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 
+// Function to validate form inputs
+function validateForm() {
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const feedback = document.getElementById("feedback").value.trim();
+    let isValid = false;
+    if (!name || !email || !feedback) {
+        alert("Please fill out all fields before submitting.");
+        return false;
+    }
+
+    return true;
+    
+}
+
+
+
+// Add event listener for the Cancel button
+document.getElementById("cancelFeedbackButton").addEventListener("click", function() {
+        window.location.href = "index.html"; // Replace with the actual path to your portfolio page
+});
 // Function to show "Thank you" message and navigate to the portfolio page
 function showThankYouAndNavigate() {
-    alert("Thank you for your feedback!");
-    window.location.href ="index.html"; // Replace with the actual path to your portfolio page
+    if (validateForm()) {
+        alert("Thank you for your feedback!");
+        window.location.href = "index.html"; // Replace with the actual path to your portfolio page
+    }
 }
 
 // Example usage: Add an event listener to a button on the feedback form page

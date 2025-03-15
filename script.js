8c1b1268bffc9ba99c9bd1dd0a2e961fa64a72d1
rksudhir <rksudhir@hotmail.com>
2025-03-14 23:50:41 -0400

    Corrected the index page name

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Function to show "Thank you" message and navigate to the portfolio page
 function showThankYouAndNavigate() {
     alert("Thank you for your feedback!");
-    window.location.href = "portfolio.html"; // Replace with the actual path to your portfolio page
+    window.location.href ="index.html"; // Replace with the actual path to your portfolio page
 }
 
 // Example usage: Add an event listener to a button on the feedback form page
